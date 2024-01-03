3e8af2c4f57576f737dcc94eab3205dd642ee189
DallinEdmunds <51213681+DallinEdmunds@users.noreply.github.com>
2024-01-02 21:41:13 -0700

    Form Validator Complete
    completed the form validator.
    next step is adding more functionality to it like a backend. but that's for later.

--- 0_LandingPage/1_FormValidator/script.js
+++ 0_LandingPage/1_FormValidator/script.js
@@ -20,10 +20,50 @@ function showSuccess(input) {
 }
 
 //check email is valid
-function isValidEmail(email) {
-    var re = /\S+@\S+\.\S+/;
-  return re.test(email);
+function checkEmail(input) {
+    const re = /\S+@\S+\.\S+/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input); 
+  } else {
+    showError(input, "email is not valid")
+  }
     
+    
+}
+//check required fields
+function checkRequired(inputArr) {
+inputArr.forEach(function(input) {
+    if(input.value.trim() === '') {
+        showError(input, `${getFieldName(input)} is required`); 
+    } else {
+        showSuccess(input);
+    }
+});
+}
+
+//check input length
+function checkLength(input, min, max) {
+    if(input.value.length <min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if(input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+//check passwords match
+function checkPasswordsMatch(input1, input2) {
+    if(input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match'); 
+    } else {
+        showSuccess(input2);
+    }
+}
+
+//get fieldname
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 // Event Listeners
@@ -33,29 +73,10 @@ form.addEventListener('submit', function(e) {
 
 e.preventDefault();
 
-    if(username.value === "") {
-        showError(username, 'Username is required')
-    } else {
-        showSuccess(username);
-    }
+checkRequired([username, email, password, password2]);
+checkLength(username, 3, 15);
+checkLength(password, 6, 25);
+checkEmail(email);
+checkPasswordsMatch(password, password2);
 
-    if(email.value === "") {
-        showError(email, 'Email is required')
-    } else if(!isValidEmail(email.value)) {
-        showError(email, 'Email is not valid') 
-    } else {
-        showSuccess(email);
-    }
-
-    if(password.value === "") {
-        showError(password, 'Password is required')
-    } else {
-        showSuccess(password);
-    }
-
-    if(password2.value === "") {
-        showError(password2, "Password 2 is required")
-    } else {
-        showSuccess(password2);
-    }
 });
